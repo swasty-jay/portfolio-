e236efc1dfe4c497a8fa53f9d8d94fa7b7026c86
Daniel Amekpoagbe <amekpoagbedaniel@gmail.com>
2025-02-18 21:34:21 +0000

    styling

--- src/features/Skills.jsx
+++ src/features/Skills.jsx
@@ -112,7 +112,7 @@ const Skills = () => {
         </div>
 
         {/* Experience & Education Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8  px-4 sm:px-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8  ">
           <div>
             <h3 className="text-2xl font-bold text-blue-400 flex items-center mb-4">
               <FaBriefcase className="mr-2" /> My Experience
