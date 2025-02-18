467f8c302d3c7c0ccdf726f0d51153f7eeabec1c
Daniel Amekpoagbe <amekpoagbedaniel@gmail.com>
2025-02-18 21:31:47 +0000

    styling

--- src/features/Skills.jsx
+++ src/features/Skills.jsx
@@ -65,7 +65,7 @@ const education = [
 
 const Skills = () => {
   return (
-    <section className="py-12 bg-gray-900 text-white sm:px-4">
+    <section className="py-12 bg-gray-900 text-white px-4 sm:px-6">
       <div className="container mx-auto text-center">
         <motion.h2
           className="text-3xl font-bold text-blue-400 mb-6"
@@ -112,7 +112,7 @@ const Skills = () => {
         </div>
 
         {/* Experience & Education Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8  px-4 sm:px-6">
           <div>
             <h3 className="text-2xl font-bold text-blue-400 flex items-center mb-4">
               <FaBriefcase className="mr-2" /> My Experience
